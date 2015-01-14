80440462a97a140a23aa5ca3b72d373ee3105a14
andela-aadesokan <adewunmi.adesokan@andela.co>
2015-01-14 13:33:33 +0100

    adjusted port number

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
 };
+
+app.set('port', (process.env.PORT || 8080));
 app.use(allowCrossDomain);
 
 
@@ -38,7 +40,15 @@ router(routes);
 
 
 
-app.listen((8080), function() {
- console.log('running....');
+
+app.listen(app.get('port'), function() {
+ console.log("app is running at localhost:" + app.get('port'));
 });
+
+
+
+
+
+
+
 
